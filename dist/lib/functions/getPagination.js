2bc7e031ff0c6fb4fc1bac347d3ec2fed16e546a
dlwndks9436 <dlwndks9436@naver.com>
2022-04-14 14:46:19 +0900

    Remove dist folder

--- dist/lib/functions/getPagination.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPagination = void 0;
-const getPagination = (page, size) => {
-    const limit = size ? +parseInt(size, 10) : 10;
-    const offset = page ? parseInt(page, 10) * limit : 0;
-    return { limit, offset };
-};
-exports.getPagination = getPagination;
