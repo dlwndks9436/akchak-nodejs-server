0b739796f39d57e7fa15a658d752d0527854bbb1
dlwndks9436 <dlwndks9436@naver.com>
2022-04-27 16:56:47 +0900

    Add get books controller

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { sequelize } from "../model";
 import { StatusCodes } from "http-status-codes";
 import Book from "../model/book";
+import { Op } from "sequelize";
 
 export const addBook = async (req: Request, res: Response) => {
   try {
@@ -22,12 +23,20 @@ export const addBook = async (req: Request, res: Response) => {
   }
 };
 
-// export const getBooks = async (req: Request, res: Response) => {
-//  try {
-//    const { limit, title } = req.query;
-//    await
-//  } catch (err) {
-//   console.log(err);
-
-//  }
-// }
+export const getBooks = async (req: Request, res: Response) => {
+  try {
+    const { limit, title } = req.query;
+    if (title?.length === 0) {
+      res.status(StatusCodes.BAD_REQUEST).end();
+    } else {
+      const books = await Book.findAll({
+        where: { title: { [Op.substring]: title as string } },
+        limit: parseInt(limit as string),
+      });
+      res.status(StatusCodes.OK).send(books);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end(err);
+  }
+};
--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { addBook } from "../controllers/book";
+import { addBook, getBooks } from "../controllers/book";
 import { verifyAccessToken, bookInputValidator } from "../middleware";
 
 export const bookRouter = Router();
 
 bookRouter.post("/", verifyAccessToken, bookInputValidator, addBook);
 
-bookRouter.get("/", verifyAccessToken);
+bookRouter.get("/", verifyAccessToken, getBooks);
