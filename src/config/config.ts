bb95c3c5af65e9e26e3f8d0eb9845485eff7f7b4
dlwndks9436 <dlwndks9436@naver.com>
2022-04-27 13:07:24 +0900

    Change sequelize config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,12 +17,4 @@ export default {
     host: env.RDS_HOSTNAME || "localhost",
     dialect: env.DATABASE_DIALECT || "mysql",
   },
-  // username:
-  //   env.NODE_ENV === "development" ? env.DATABASE_USERNAME : env.RDS_USERNAME,
-  // password:
-  //   env.NODE_ENV === "development" ? env.DATABASE_PASSWORD : env.RDS_PASSWORD,
-  // database:
-  //   env.NODE_ENV === "development" ? env.DATABASE_NAME : env.RDS_DB_NAME,
-  // host: env.NODE_ENV === "development" ? env.DATABASE_HOST : env.RDS_HOSTNAME,
-  // dialect: env.DATABASE_DIALECT,
 };
